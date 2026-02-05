02195ca4c3cbf6dc3360cd59b5da5a05843ff81f
Nimdula Perera <nimdulaperera@Nimdulas-MacBook-Pro.local>
2026-02-05 16:20:46 -0500

    third commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+data = {
+    'accounts': [
+    {'id': 1, 'name': 'Account 1', 'balance': 1000},
+    {'id': 2, 'name': 'Account 2', 'balance': 2500},
+    {'id': 3, 'name': 'Account 3', 'balance': 500}
+    ]
+}
+
 def homepage(request):
     return HttpResponse("Welcome to the homepage!")
 
 def accounts(request):
-    return render(request, 'accounts/accounts.html', {'accounts': ['Account 1', 'Account 2', 'Account 3']})
+    return render(request, 'accounts/accounts.html', data)
